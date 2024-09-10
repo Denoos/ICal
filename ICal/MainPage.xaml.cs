afef3f8fa8f4140934ca33870f1aa4ad58a52e1a
Данил Тимофеев <danil2006timofeev@gmail.com>
2024-09-10 11:25:40 +1000

    ЗАлупа негра

--- ICal/MainPage.xaml.cs
+++ ICal/MainPage.xaml.cs
@@ -1,14 +1,136 @@
-﻿namespace ICal
+﻿using System.Diagnostics.Tracing;
+
+namespace ICal
 {
     public partial class MainPage : ContentPage
     {
+
+        long result = 0;
+
         public MainPage()
         {
             InitializeComponent();
         }
 
-        
+        private void PercentBtn_Clicked(object sender, EventArgs e)
+        {
 
+        }
+
+        private void CeBtn_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CBtn_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BackBtn_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ReverseBtn_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PowerBtn_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void AntiPowerBtn_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void DelitBtn_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void SevenBtn_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void EightBtn_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void NineBtn_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void YmnogitBtn_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void FourBtn_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void FiveBtn_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void SixBtn_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void MinusBtn_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void OneBtn_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TwoBtn_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ThreeBtn_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PlusBtn_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void AntiXBtn_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ZeroBtn_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void DesyatieBtn_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ResultBtn_Clicked(object sender, EventArgs e)
+        {
+
+        }
     }
 
 
